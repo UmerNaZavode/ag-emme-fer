5555c17c372306087cd89656e190a90dd2b08f8a
gaba-work <malkocmax@gmail.com>
2025-07-28 18:10:45 +0300

    upd: fixed desc

--- template-parts/home/creations.php
+++ template-parts/home/creations.php
@@ -1,15 +1,15 @@
 <?php
-$creations = get_field('creations');
-$logo = $creations['logo'];
-$title = $creations['title'];
-$description = $creations['description'];
+$creations_info = get_field('creations_info');
+$logo = $creations_info['logo'];
+$title = $creations_info['title'];
+$description = $creations_info['description'];
 ?>
 <div class="container">
   <div class="creations">
     <img src="<?php echo $logo['url']; ?>" alt="<?php echo $logo['alt']; ?>" />
     <div class="creations__wrapper">
       <h3 class="creations__title title"><?php echo $title; ?></h3>
-      <p class="creations__description"><?php echo $description; ?></p>
+      <div class="creations__description"><?php echo $description; ?></div>
     </div>
   </div>
 </div>
