8ec34943e0747bf68d3e5fe015c22c59cf33a33e
gaba-work <malkocmax@gmail.com>
2025-07-28 17:51:28 +0300

    feat: finished home intro

---
+++ template-parts/home/home-intro.php
@@ -0,0 +1,13 @@
+<?php
+$home_intro = get_field('home_intro');
+$label = $home_intro['label'];
+$title = $home_intro['title'];
+$image = $home_intro['image'];
+?>
+<div class="home-intro">
+  <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+  <div class="container">
+    <h3 class="home-intro__label"><?php echo $label; ?></h3>
+    <h2 class="home-intro__title"><?php echo $title; ?></h2>
+  </div>
+</div>
