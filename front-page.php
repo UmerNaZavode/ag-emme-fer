92b15b9f467f531fa74592d822cfffed3ff885e5
gaba-work <malkocmax@gmail.com>
2025-07-30 13:53:42 +0300

    feat: home intro adaptation

--- front-page.php
+++ front-page.php
@@ -6,7 +6,7 @@
 get_header();
 ?>
 <?php get_template_part('template-parts/home/home-intro');?>
-<?php //get_template_part('template-parts/home/creations');?>
+<?php get_template_part('template-parts/home/creations');?>
 <?php //get_template_part('template-parts/home/creations-card');?>
 <?php //get_template_part('template-parts/home/masters');?>
 <?php //get_template_part('template-parts/home/contact-form');?>
