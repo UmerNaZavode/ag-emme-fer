e1213dfd19a38e3508268a9886d65260f1a5d8a9
gaba-work <malkocmax@gmail.com>
2025-07-30 10:57:13 +0300

    upd: fixed plus icon in creations card also fixed header menu and warnings in the form

--- template-parts/home/creations-card.php
+++ template-parts/home/creations-card.php
@@ -17,7 +17,9 @@ $items = $creations_card['items'];
       <div class="creations-card__wrapper">
         <div class="creations-card__content">
           <h4 class="creations-card__title"><?php echo $title; ?></h4>
-        <button type="submit" class="creations-card__button btn btn--small"></button>
+        <button type="submit" class="creations-card__button btn">
+          <span></span>
+        </button>
         </div>
       </div>
     </div>
