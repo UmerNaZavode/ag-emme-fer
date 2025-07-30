f062100d53f79d7bf70cc113788f8678ee663a27
gaba-work <malkocmax@gmail.com>
2025-07-30 14:59:40 +0300

    feat: finished creations adaptation

--- template-parts/home/creations.php
+++ template-parts/home/creations.php
@@ -4,12 +4,10 @@ $logo = $creations_info['logo'];
 $title = $creations_info['title'];
 $description = $creations_info['description'];
 ?>
-<div class="container">
-  <div class="creations">
+  <div class="creations container">
     <img src="<?php echo $logo['url']; ?>" alt="<?php echo $logo['alt']; ?>" />
     <div class="creations__wrapper">
       <h3 class="creations__title title"><?php echo $title; ?></h3>
       <div class="creations__description"><?php echo $description; ?></div>
     </div>
   </div>
-</div>
