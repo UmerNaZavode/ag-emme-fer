21771043e1d6386e85c5922e5f48e39d3d050d37
gaba-work <malkocmax@gmail.com>
2025-07-30 13:45:20 +0300

    feat: finished footer adaptation

--- front-page.php
+++ front-page.php
@@ -6,8 +6,8 @@
 get_header();
 ?>
 <?php get_template_part('template-parts/home/home-intro');?>
-<?php get_template_part('template-parts/home/creations');?>
-<?php get_template_part('template-parts/home/creations-card');?>
-<?php get_template_part('template-parts/home/masters');?>
-<?php get_template_part('template-parts/home/contact-form');?>
+<?php //get_template_part('template-parts/home/creations');?>
+<?php //get_template_part('template-parts/home/creations-card');?>
+<?php //get_template_part('template-parts/home/masters');?>
+<?php //get_template_part('template-parts/home/contact-form');?>
 <?php get_footer(); ?>
