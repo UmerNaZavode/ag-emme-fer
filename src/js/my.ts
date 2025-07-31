6fa4e4fd5c5404c942d874217774056186a48612
gaba-work <malkocmax@gmail.com>
2025-07-31 13:46:17 +0300

    feat: home intro button function created

--- src/js/my.ts
+++ src/js/my.ts
@@ -1,16 +1,33 @@
-import "../vue/vue-app.js";
-import creationCardToggleAnimation from "./modules/creationCardToggleAnimation.js";
-import fixedHeader from "./modules/header/fixed-header";
-import streetMap from "./modules/maps/streetMap.js";
-import { mainMenu } from "./modules/menu";
+import '../vue/vue-app.js';
+import creationCardToggleAnimation from './modules/creationCardToggleAnimation.js';
+import fixedHeader from './modules/header/fixed-header';
+// import streetMap from './modules/maps/streetMap.js';
+import { mainMenu } from './modules/menu';
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener('DOMContentLoaded', function () {
   mainMenu();
   fixedHeader();
   // streetMap();
-  const creations_card =   document.querySelector('.creations-card');
-  if(creations_card){
-    creationCardToggleAnimation()
+  const creations_card = document.querySelector('.creations-card');
+  if (creations_card) {
+    creationCardToggleAnimation();
+  }
+
+  const button = document.getElementById('scroll-btn');
+  const target = document.querySelector('.creations');
+
+  if (button && target) {
+    button.addEventListener('click', function () {
+      const offset =
+        7 * parseFloat(getComputedStyle(document.documentElement).fontSize); // 5rem
+      const top =
+        target.getBoundingClientRect().top + window.pageYOffset - offset;
+
+      window.scrollTo({
+        top,
+        behavior: 'smooth'
+      });
+    });
   }
 });
 
