7700cb71217c0a98a72d16b3d598074592d1a913
gaba-work <malkocmax@gmail.com>
2025-08-01 13:09:43 +0300

    upd: fixed creations-card adaptation

--- template-parts/home/creations-card.php
+++ template-parts/home/creations-card.php
@@ -12,19 +12,22 @@ $items = $creations_card['items'];
     $button_text = $item['button_text'];
     $button_url = $item['button_url'];
     ?>
-    <div class="creations-card__inner <?php //echo $k === 0 ? 'active' : null; ?>">
+    <div class="creations-card__inner <?php //echo $k === 0 ? 'active' : null; 
+                                      ?>">
       <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
       <div class="creations-card__wrapper">
         <header class="creations-card__header">
           <h4 class="creations-card__title"><?php echo $title; ?></h4>
           <button type="button" class="creations-card__button btn btn-open"><span></span></button>
         </header>
-        <div class="creations-card__description">
-          <?php echo $description; ?>
+        <div class="creations-card__content">
+          <div class="creations-card__description">
+            <?php echo $description; ?>
+          </div>
+          <footer class="creations-card__footer">
+            <button type="button" class="creations-card__button--alt btn"><a href="<?php echo $button_url; ?>" class="creations-card__link btn__link"><?php echo $button_text; ?></a></button>
+          </footer>
         </div>
-        <footer class="creations-card__footer">
-          <button type="button" class="creations-card__button--alt btn"><a href="<?php echo $button_url; ?>" class="creations-card__link btn__link"><?php echo $button_text; ?></a></button>
-        </footer>
       </div>
     </div>
   <?php endforeach; ?>
